1365b53a24950c89b93abc04b68b73c9dd261f25
Femmy <fegomson@gmail.com>
2022-09-30 14:38:28 -0500

    Adds morgan for dev

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectDB = require('./config/connectDB');
+const morgan = require('morgan');
 
 const app = express();
 
 dotenv.config();
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+const api = process.env.API_URL;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the backend server');
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get(`${api}/products`, (req, res) => {
+  const products = {
+    id: 1,
+    name: 'Product 1',
+    price: 100,
+    quantity: 1,
+  };
+
+  res.send(products);
 });
 
-const PORT = process.env.PORT || 3001;
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+
+  res.send(newProduct);
+});
+
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   await connectDB();
